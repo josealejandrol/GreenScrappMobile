ce21a29311a2e9cee5d24de511e9854e5cdb88e5
josealejandrol <carmens_321@outlook.com>
2020-03-19 10:17:17 -0600

    Estructura del proyecto e instalacion de ReactNavigation

---
+++ app/screens/SplashScreen/SplashScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function SplashScreen() {
+    return(
+        <SafeAreaView style={{flex: 1}}>
+            <Text>SplashScreen</Text>
+        </SafeAreaView>
+    )
+}
