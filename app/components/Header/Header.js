91c65e41e41ea8bd3e9b299a458d3c9970a711e5
SergioSosaGonzalez <sergioarsosa95@gmail.com>
2020-03-27 11:26:04 -0600

    Update app/components/Header/Header.js
    Co-Authored-By: Jesus Manuel Garcia Torres <garciatjm@gmail.com>

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -6,21 +6,22 @@ import styleHeader from '@components/Header/style'
 
 
 export default function Header(props) {
+   const {back, icoback, logo, alternativeButton, icoplus, alternativeText, title } = props;
     return (
        <View style={styleHeader.container}>
             <ImageBackground  source={require('../../assets/fondo.png')} style={styleHeader.background}>
-                <TouchableOpacity  style={styleHeader.back} onPress={props.back}>
-                    <Image source={props.icoback} style={{width: 25, height: 25}}/>
+                <TouchableOpacity  style={styleHeader.back} onPress={back}>
+                    <Image source={icoback} style={{width: 25, height: 25}}/>
                 </TouchableOpacity>
                 <View style={styleHeader.logocontainer}>
-                    <Image source={props.logo} style={{width: 115, height: 60}}/>
+                    <Image source={logo} style={{width: 115, height: 60}}/>
                 </View>
-                <TouchableOpacity style={styleHeader.plus} onPress={props.alternativeButton}>
-                    <Image source={props.icoplus}/>
-                    <Text style={styleHeader.textalternative}>{props.alternativeText}</Text>
+                <TouchableOpacity style={styleHeader.plus} onPress={alternativeButton}>
+                    <Image source={icoplus}/>
+                    <Text style={styleHeader.textalternative}>{alternativeText}</Text>
                 </TouchableOpacity>
             </ImageBackground>
-            <Text style={styleHeader.title}>{props.title}</Text>
+            <Text style={styleHeader.title}>{title}</Text>
        </View>
     )
 }
